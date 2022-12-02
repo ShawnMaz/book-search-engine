cc6a8834c0e3ce51c5a75416125dde4ad69eff98
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-12-01 21:25:15 -0500

    updated the server.js file to work with the Apollo server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer(
+  {
+    typeDefs,
+    resolvers,
+    context:authMiddleware
+  }
+);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +24,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// if the user navigates to a url that does not exist, then they will be redirected to the index page
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  // integrate Apollo server with the Express application as a middleware
+  server.applyMiddleware({app});
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+}
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
