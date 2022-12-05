df5e78c333526c86ef018be515f2c0f00590b3ff
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-12-05 12:26:37 -0500

    removed incorrect destructuring for args from resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
     Query:{
         me: async (parent, args, context) => {
             if(context.user){
-                const userData = await User.finOne({_id:context.user._id})
+                const userData = await User.findOne({_id:context.user._id})
                     .select('-__v -password');
                 return userData;
             }
@@ -29,12 +29,13 @@ const resolvers = {
             const token = signToken(user);
             return {token, user};
         },
-        addUser: async(parent, {args}) => {
+        addUser: async(parent, args) => {
             const user = await User.create(args);
+            console.log(user);
             const token = signToken(user);
             return {token, user};
         },
-        saveBook: async(parent, {args}, context) => {
+        saveBook: async(parent, args, context) => {
             if(context.user){
                 const book = await Book.create({...args});
                 const updatedUser = await User.findByIdAndUpdate(
